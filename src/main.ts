df36c531359b7b7fe2f1732a60c4339b08493eb7
zy_H <hzy18023@gmail.com>
2022-06-08 14:58:22 +0900

    Revert "init"
    This reverts commit ab3acc17489c344c6553d33c81e487c7cc56a4d8.

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App)
-    .use(ElementPlus, { locale: zhCn })
-    .mount('#app')
